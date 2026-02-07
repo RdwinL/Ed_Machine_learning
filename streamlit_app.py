2a8413802d27df8016266ff16a91de968efbc193
RdwinL <eddylaswai@gmail.com>
2026-02-07 22:33:31 +0300

    Fix indentation for expander context manager

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ st.title('Machine Learning App')
 
 st.info('This build a machine learning model')
 
-with st.expander('Data')
+with st.expander('Data'):
  st.write(**Raw data**)
  df = pd.read_csv('https://raw.githubusercontent.com/RdwinL/Ed_Machine_learning/refs/heads/master/crop_yield_data.csv')
 df
