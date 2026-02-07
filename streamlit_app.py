c3656b5e8aab34b54cb8a218c312c644fe88a553
RdwinL <eddylaswai@gmail.com>
2026-02-07 22:32:56 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,8 @@ import pandas as pd
 st.title('Machine Learning App')
 
 st.info('This build a machine learning model')
-df = pd.read_csv('https://raw.githubusercontent.com/RdwinL/Ed_Machine_learning/refs/heads/master/crop_yield_data.csv')
+
+with st.expander('Data')
+ st.write(**Raw data**)
+ df = pd.read_csv('https://raw.githubusercontent.com/RdwinL/Ed_Machine_learning/refs/heads/master/crop_yield_data.csv')
 df
