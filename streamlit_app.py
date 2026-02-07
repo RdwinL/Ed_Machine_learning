37f398aa7562220b8d24df4266da35d285116630
RdwinL <eddylaswai@gmail.com>
2026-02-07 22:35:08 +0300

    Fix typo in data section header

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,6 @@ st.title('Machine Learning App')
 st.info('This build a machine learning model')
 
 with st.expander('Data'):
- st.write(**'Raw data'**)
+ st.write(**'Raw data**')
  df = pd.read_csv('https://raw.githubusercontent.com/RdwinL/Ed_Machine_learning/refs/heads/master/crop_yield_data.csv')
 df
