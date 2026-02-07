3a88d86753d098862030910f3577aa398c8de2b0
RdwinL <eddylaswai@gmail.com>
2026-02-07 22:30:38 +0300

    Show DataFrame after reading crop yield data
    Display the DataFrame after loading the CSV.

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,4 @@ st.title('Machine Learning App')
 
 st.info('This build a machine learning model')
 df = pd.read_csv('https://raw.githubusercontent.com/RdwinL/Ed_Machine_learning/refs/heads/master/crop_yield_data.csv')
+df
