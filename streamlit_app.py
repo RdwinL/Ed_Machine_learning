e98881b074778a88272456378899732d11e73516
RdwinL <eddylaswai@gmail.com>
2026-02-07 22:29:39 +0300

    Add pandas import and load dataset from CSV

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit as st
+import pandas as pd
 
 st.title('Machine Learning App')
 
 st.info('This build a machine learning model')
+df = pd.read_csv('https://raw.githubusercontent.com/RdwinL/Ed_Machine_learning/refs/heads/master/crop_yield_data.csv')
